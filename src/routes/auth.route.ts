886cdca8cf74d768dc61ef0379b1d66c9df62561
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-09-08 21:57:28 +0300

    creating AuthRoute

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import AuthController from "../controllers/auth.controller";
+import { Routes } from "../interfaces/route.interface";
+
+class AuthRoute implements Routes {
+  public path = "/";
+  public router = Router();
+  public authController = new AuthController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`${this.path}signup`, this.authController.signUp);
+    this.router.post(`${this.path}login`, this.authController.logIn);
+  }
+}
+
+export default AuthRoute;
