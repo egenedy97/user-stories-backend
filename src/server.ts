fba3b59d0cf71d41be992804596aa6df304d696b
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-09-11 18:03:56 +0300

    build user route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import App from "./app";
 import AuthRoute from "./routes/auth.route";
 import ProjectRoute from "./routes/project.route";
 import TaskRoute from "./routes/task.route";
+import UserRoute from "./routes/user.route";
 
-const app = new App([new AuthRoute(), new ProjectRoute(), new TaskRoute()]);
+const app = new App([
+  new AuthRoute(),
+  new ProjectRoute(),
+  new TaskRoute(),
+  new UserRoute(),
+]);
 app.listen();
