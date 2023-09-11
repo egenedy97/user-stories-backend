b80dc76f090d8d7a8d8434c9711a7433e1cc7532
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-09-11 17:33:07 +0300

    creating TaskRoute

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import App from "./app";
 import AuthRoute from "./routes/auth.route";
 import ProjectRoute from "./routes/project.route";
+import TaskRoute from "./routes/task.route";
 
-const app = new App([new AuthRoute(), new ProjectRoute()]);
+const app = new App([new AuthRoute(), new ProjectRoute(), new TaskRoute()]);
 app.listen();
