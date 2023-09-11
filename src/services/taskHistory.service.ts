1618e2043b336fdb9a9c531553ae410dea3eb90f
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-09-11 17:32:45 +0300

    creating taskHistory

---
+++ src/services/taskHistory.service.ts
@@ -0,0 +1,35 @@
+import { HttpException } from "../exceptions/HttpException";
+import { prisma } from "../database";
+import { TaskHistory } from "@prisma/client";
+
+class TaskHistoryService {
+  public taskHistory = prisma.taskHistory;
+
+  public async createTaskHistory(createdTask: {
+    id: number;
+    status: "ToDo" | "InProgress" | "InQA" | "Done" | "Deployed" | "Blocked";
+    previousStatus?:
+      | "ToDo"
+      | "InProgress"
+      | "InQA"
+      | "Done"
+      | "Deployed"
+      | "Blocked";
+
+    changedById: number;
+  }): Promise<TaskHistory> {
+    const createdTaskHistory: TaskHistory = await this.taskHistory.create({
+      data: {
+        taskId: +createdTask.id,
+        currentStatus: createdTask.status,
+        changedById: createdTask.changedById,
+        previousStatus: createdTask.previousStatus,
+      },
+    });
+    return createdTaskHistory;
+  }
+
+  //   public async getTaskHistory();
+}
+
+export default TaskHistoryService;
